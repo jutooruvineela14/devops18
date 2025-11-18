fa30725de984a02be6ce9fa15269e4278873f4d3
jutooruvineela14 <vineelajutooru@gmail.com>
2025-11-18 11:00:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-0ecb62995f68bb549"
+    image_id           = "ami-0cae6d6fe6048ca2c"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
     key_name = "DOCKER-KP"
